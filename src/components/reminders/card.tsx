1a3db8d93022158ed39343bb111f5c4b351ab918
Vasily Mitronov <vasmitr95@gmail.com>
2024-12-22 11:50:16 +0300

    Add remove button

--- src/components/reminders/card.tsx
+++ src/components/reminders/card.tsx
@@ -1,7 +1,9 @@
+import { Trash2 } from "lucide-react";
 import { Reminder, state } from "@/data/reminders";
 import { Card, CardContent, CardFooter, CardTitle } from "../ui/card";
 import { Checkbox } from "../ui/checkbox";
 import { Badge } from "../ui/badge";
+import { Button } from "@/components/ui/button";
 
 interface ReminderProps {
   reminder: Reminder;
@@ -12,14 +14,27 @@ export default function ReminderCard({ reminder }: ReminderProps) {
     state.toggleIsDone(reminder.id);
   };
 
+  const handleRemove = (reminder: Reminder) => {
+    state.deleteReminder(reminder.id);
+  };
+
   return (
     <Card className="rounded-none px-2 py-2">
-      <CardTitle className="text-xs font-normal text-gray-800">
-        {reminder.title}
+      <CardTitle className="text-xs font-normal text-gray-800 flex justify-between w-full">
+        <p>{reminder.title}</p>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="h-8 w-8 text-muted-foreground hover:text-red-500"
+          onClick={() => handleRemove(reminder)}
+        >
+          <Trash2 />
+        </Button>
       </CardTitle>
       <CardContent className="p-0 flex justify-between text-gray-400 text-xs">
         <p>{reminder.notes || "Notes"}</p>
         <Checkbox
+          className="mt-2 mr-2"
           checked={reminder.isDone}
           onCheckedChange={() => handleCheck(reminder)}
         />
