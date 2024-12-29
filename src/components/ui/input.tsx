d40843e3c762094e2da8e5f36e28703e653d88d2
Vasily Mitronov <vasmitr95@gmail.com>
2024-12-29 11:20:26 +0300

    Fix input border

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full bg-background px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none  text-slate-800 border-b-2 border-b-slate-400 focus:border-b-4 focus:border-blue-400",
+          "flex h-10 w-full rounded-none bg-background px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none  text-slate-800 border-b-2 border-b-slate-400 focus:border-b-4 focus:border-blue-400",
           className
         )}
         ref={ref}
