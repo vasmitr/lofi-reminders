280971cafd64d768bcd81aa142de8c31ee2501f3
Vasily Mitronov <vasmitr95@gmail.com>
2024-12-29 11:20:17 +0300

    Setup precache

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 
 import LV from "@lo-fi/local-vault/bundlers/vite";
 
 export default defineConfig({
-  plugins: [LV(), react()],
+  plugins: [
+    LV(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
